14ee5616a493c7205ce476ef18a7c27e0ffca3a7
Summer Unsinn <sunsinn@radcliffe.harvard.edu>
2015-09-20 12:29:19 -0400

    Finished random word selection and breaking for lunch

--- index.php
+++ index.php
@@ -8,10 +8,11 @@
     <h2>XKCD-Style Password Generator</h2>
     <h3>CSCI E-15 Project 2</h3>
     <div id="password">
+      <?php echo $password ?>
     </div>
     <div>
     <form method="POST" action="index.php">
-      <input type="range" name="words" min="4" max="12" value="4">Number of words (4-12)<br>
+      <input type="range" name="wordsnum" min="4" max="12" value="4">Number of words (4-12)<br>
       <input type="checkbox" name="number" value="num">Include number<br>
       <input type="checkbox" name="symbol" value="sym">Include symbol<br>
 
@@ -19,10 +20,6 @@
     </form>
     Inspired by <a href = "http://xkcd.com/936/">Randall Munroe's XKCD</a><br>
     <img src = "password_strength.png" />
-
-    <pre>
-    <?php print_r($_POST); ?>
-</pre>
   </div>
   </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,10 +1,20 @@
 <?php
-
+  # Builds an array from a CSV list of 1000 most common English words
   $file = fopen("1000-words-csv.csv","r");
   while(!feof($file))
   {
     $words[] = fgetcsv($file, 1000, ",");
   }
   fclose($file);
+
+# adds first random word to password string
+$r = array_rand($words);
+$password = $words[$r][0];
+
+$numwords = (int)$_POST["wordsnum"];
+for ($i = 1; $i < $numwords; $i++)  {
+    $r = array_rand($words);
+    $password .= "-".$words[$r][0];
+}
 
 ?>
